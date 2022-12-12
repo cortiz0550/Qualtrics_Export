084b0c872cde79646304507bb88e06b53444b516
Christopher Ortiz <53792120+cortiz0550@users.noreply.github.com>
2022-12-12 13:26:59 -0800

    Update date_selector.py
    Added more logic to only select surveys that need reporting. For small numbers of surveys, not a huge change, but can save time for larger numbers of surveys. Also added data to adjust export settings.

--- Automation/date_selector.py
+++ Automation/date_selector.py
@@ -3,12 +3,14 @@
 
 import datetime
 import pandas as pd
+import paths
 
 date_cols = ["Start_Date", "End_Date"]
-path = # Path to Report_Schedule.csv
+path = paths.report_schedule
 
 report_schedule_df = pd.read_csv(path, parse_dates=date_cols)
 today = datetime.date.today()
+# print(today)
 
 weekday_mapping = {day: index for index, day in enumerate((
 	"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))}
@@ -19,24 +21,23 @@ for i in range(len(report_schedule_df['Survey_ID'])):
 
 	start_date = report_schedule_df['Start_Date'][i] if not pd.isnull(report_schedule_df['Start_Date'][i]) else datetime.date.min
 	end_date = report_schedule_df['End_Date'][i] if not pd.isnull(report_schedule_df['End_Date'][i]) else datetime.date.max
-	print(start_date, end_date)
 
-	if (start_date <= today) and (today <= end_date):
-		days = report_schedule_df['Schedule'][i].split(";")
+	# This makes sure the survey actually needs reporting. It may just be open but we don't need regular downloads.
+	if report_schedule_df['Reporting_Necessary'][i]:
+		if (start_date <= today) and (today <= end_date):
+			days = report_schedule_df['Schedule'][i].split(";")
 
-		selected_weekdays = []
+			selected_weekdays = []
 
-		for day in days:
-			selected_weekdays.append(weekday_mapping.get(day,day))
-		print(selected_weekdays)
+			for day in days:
+				selected_weekdays.append(weekday_mapping.get(day,day))
 
-	print(today.weekday())
-	if today.weekday() in selected_weekdays:
+		if today.weekday() in selected_weekdays:
 
-		surveys.append({
-			"survey_id": report_schedule_df['Survey_ID'][i],
-			"survey_name": report_schedule_df['Survey_Name'][i],
-			"schedule": days})
-
-
-
+			surveys.append({
+				"survey_id": report_schedule_df['Survey_ID'][i],
+				"survey_name": report_schedule_df['Survey_Name'][i],
+				"schedule": days,
+				"file_format": report_schedule_df['File_Formats'][i],
+				"split_columns": report_schedule_df['Split_Columns'][i],
+				"use_labels": report_schedule_df['Numeric_Recode'][i]})
